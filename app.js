d06c4848bcc7008ce4f76ff7f961e238a8e934c4
nash <contact@nash-dev.com>
2012-12-01 20:05:28 +0900

    設定ファイルへのパスをしっかり解決するように

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var fs = require('fs'),
     mc = require('./mc');
 
+var path = __filename.split('/');
+path.pop();
+path = path.join('/') + '/settings.json';
+
 try {
-	var settings = JSON.parse(fs.readFileSync('settings.json'));
+	var settings = JSON.parse(fs.readFileSync(path));
 } catch(e) {
 	console.log('settings.json not found or invalid.');
 	process.exit(1);
